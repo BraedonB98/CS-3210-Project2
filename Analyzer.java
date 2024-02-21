62715dca3943b7c67083bc324ee80ed07e62b439
Braedon Bellamy <braedonb98@gmail.com>
2024-02-21 15:28:35 -0700

    added to Analyzer(main code) with functions it will need to call through out the assignment

--- Analyzer.java
+++ Analyzer.java
@@ -1,7 +1,18 @@
+import Headers.java;
+import Indentation.java;
+import OutputFormatter.java;
+import PrintCounter.java;
+import PythonParser.java;
 
 public class Analyzer { 
     public static void main(String[] args){
-
+        String[] pythonParsed = PythonParse();//get parsed python file from PaythonParse File
+        Indentation.check(pythonParsed);//check indentation of Python file
+        Indentation.fix(pythonParsed)//Fixed indentation in python file
+        Headers.check(pythonParsed);//check function headers syntax
+        Headers.fix(pythonParsed);//fix function headers syntax
+        PrintCounter.count(pythonParsed);//Count how many times print is used(as a keyword)
+        OutputFormatter(pythonParsed);//output origional input program, output program, and the number of times the keyword "print is used"
 
     }
 }
