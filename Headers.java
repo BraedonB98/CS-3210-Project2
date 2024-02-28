e54076d43075adc0e8765cbcc1ea9df2b3eac066
DylanCReed <dylancreed96@gmail.com>
2024-02-28 14:01:36 -0800

    Headers code written. May need adjustments/updates.

--- Headers.java
+++ Headers.java
@@ -1,2 +1,24 @@
 // Headers.fix(pythonParsed);
 // fix function headers syntax
+
+public class Headers {
+
+    public static void findHeaders(String[] pythonLines) {
+        for(int i=0;i<numLines;i++) {
+            if(pythonLines[i].startsWith("def ")) {
+               pythonLines[i] = fixHeaders(pythonLines[i]);
+            }
+        }
+    }
+
+    public static String[] fixHeaders(String[] fixCode) {
+        if(!fixCode.endsWith("):")) {
+                fixCode += "):";
+        }
+        if(!fixCode.endsWith("(\\([^()]*\\):)")) {
+                fixCode = fixCode.replace("):", "():")
+        }
+        return fixCode;
+    }
+
+}
